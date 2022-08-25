7006a29476880cae24f562c0a48db90d409df55d
Shayan Fahmi <shayan.fahmi@gmail.com>
2022-08-25 21:02:00 +0530

    Fibonacci

--- DynamicProgrammingCSharp/DynamicProgrammingCSharp/Program.cs
+++ DynamicProgrammingCSharp/DynamicProgrammingCSharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DynamicProgrammingCSharp
 {
@@ -6,8 +7,28 @@ namespace DynamicProgrammingCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Fibonacci(46));
         }
+
+        private static int Fibonacci(int num)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            dict.Add(0, 0);
+            dict.Add(1,1);
+            return FindFibonacci(num, dict);
+        }
+
+        private static int FindFibonacci(int num, Dictionary<int, int> dict)
+        {
+            if (dict.ContainsKey(num)) return dict[num];
+
+            int fib = FindFibonacci(num - 1, dict) + FindFibonacci(num - 2, dict);
+
+            dict.Add(num, fib);
+
+            return dict[num];
+        }
+
     }
 }
 
