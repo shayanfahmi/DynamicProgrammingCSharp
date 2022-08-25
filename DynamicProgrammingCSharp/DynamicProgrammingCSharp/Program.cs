ea7c09361166dda30aece3ee29bf3e78fd925817
Shayan Fahmi <shayan.fahmi@gmail.com>
2022-08-25 21:17:08 +0530

    Tribonacci

--- DynamicProgrammingCSharp/DynamicProgrammingCSharp/Program.cs
+++ DynamicProgrammingCSharp/DynamicProgrammingCSharp/Program.cs
@@ -7,7 +7,32 @@ namespace DynamicProgrammingCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Fibonacci(46));
+            //Fibonacci
+            //Console.WriteLine(Fibonacci(46));
+
+            //Tribonacci
+            Console.WriteLine(Tribonacci(37));
+        }
+
+        private static int Tribonacci(int num)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            dict.Add(0, 0);
+            dict.Add(1, 0);
+            dict.Add(2, 1);
+            return FindTribonacci(num, dict);
+        }
+
+        private static int FindTribonacci(int num, Dictionary<int, int> dict)
+        {
+            if (dict.ContainsKey(num)) return dict[num];
+            int trib =
+                FindTribonacci(num - 1, dict) +
+                FindTribonacci(num - 2, dict) +
+                FindTribonacci(num - 3, dict);
+            dict.Add(num, trib);
+
+            return dict[num];
         }
 
         private static int Fibonacci(int num)
